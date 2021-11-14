58419d7786f2c732e53ebbaf9290844812c11faf
shnooki <82466624+vikishptra@users.noreply.github.com>
2021-11-14 18:58:37 +0700

    Add files via upload

---
+++ segitiga_terbalik.c
@@ -0,0 +1,21 @@
+#include stdio.h
+
+int main()
+{
+    int x,y,n;
+    printf(masukkan jumlah n = );
+    scanf(%d,&n);
+    for(x=1; x=n; x++)
+    {
+        for(y=1; y=x; y++)
+        {
+            printf( );
+        }
+        for(y =1; y =((n2)-((2x)-1)); y++)
+        {
+            printf(%d, x);
+        }
+        printf(n);
+    }
+    return 0;
+}
